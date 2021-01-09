13aae5f16e69353e0a31f3fe5ecc41d1440e52a5
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 12:57:03 +0900

    UPDATE; set port, use morgan

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import express, { Application } from 'express';
+import morgan from 'morgan'
+import { threadId } from 'worker_threads';
 
 export class App {
     private app: Application;
 
-    constructor(){
+    constructor(private port?: number | string){
         this.app = express();
+        this.settings();
+        this.middlewares();
     }
 
-    async listen(){
-        await this.app.listen(3000);
-        console.log('Server running on port 3000')
+    settings() {
+        this.app.set('port', this.port || process.env.PORT || 3000);
+    }
+
+    middlewares() {
+        this.app.use(morgan('dev'));
+    }
+
+    async listen() {
+        await this.app.listen(this.app.get('port'));
+        console.log('Server running on port', this.app.get('port'));
     }
 }
