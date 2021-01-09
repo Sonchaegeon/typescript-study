59a7f51532cba9b3e3400279d1cb9a5cded219c8
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 12:41:05 +0900

    CREATE; App class

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from 'express';
+
+export class App {
+    private app: Application;
+
+    constructor(){
+        this.app = express();
+    }
+
+    async listen(){
+        await this.app.listen(3000);
+        console.log('Server running on port 3000')
+    }
+}
