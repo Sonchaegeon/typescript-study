f3ce3cdc1eccc25708ac4bb532c5adfeeed37d5d
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 14:43:31 +0900

    UPDATE; post route(getPost, createPost, getPost, deletePost)

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
+import { getPosts, createPost, getPost, deletePost } from "../controllers/postController";
+
 router.route('/')
-    .get()
+    .get(getPosts)
+    .post(createPost)
+
+router.route('/:post_id')
+    .get(getPost)
+    .delete(deletePost)
 
 export default router;
